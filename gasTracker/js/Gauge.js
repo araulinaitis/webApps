5d9320855463aef0b63f7e59715a7d8f63a153e6
araulinaitis <araulinaitis@gmail.com>
2022-09-18 14:31:57 -0700

    done

--- gasTracker/js/Gauge.js
+++ gasTracker/js/Gauge.js
@@ -8,8 +8,9 @@ class Gauge {
       { x: 0, y: 0 },
       { x: 50, y: 50 },
       { x: 100, y: 100 },
-      { x: 50, y: 100},
+      { x: 50, y: 100 },
     ];
+    this.selectedPoint = null;
   }
 
   setCenter(x, y) {
@@ -33,6 +34,7 @@ class Gauge {
 
   draw() {
     const ctx = this.canvas.getContext('2d');
+    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.drawLines(ctx);
     this.draw0Label(ctx);
     this.draw100Label(ctx);
@@ -42,49 +44,116 @@ class Gauge {
     ctx.lineWidth = 2;
 
     // draw 0 line
-    ctx.strokeStyle = "#FF0000";
+    ctx.strokeStyle = '#FF0000';
     ctx.beginPath();
     ctx.moveTo(this.points[0].x, this.points[0].y);
     ctx.lineTo(this.points[1].x, this.points[1].y);
     ctx.stroke();
 
     // draw 100 line
-    ctx.strokeStyle = "#00FF00";
+    ctx.strokeStyle = '#00FF00';
     ctx.beginPath();
     ctx.moveTo(this.points[1].x, this.points[1].y);
     ctx.lineTo(this.points[2].x, this.points[2].y);
     ctx.stroke();
 
     // draw fill line
-    ctx.strokeStyle = "#0000FF";
+    ctx.strokeStyle = '#0000FF';
     ctx.beginPath();
     ctx.moveTo(this.points[1].x, this.points[1].y);
     ctx.lineTo(this.points[3].x, this.points[3].y);
     ctx.stroke();
 
     // draw points
-    for(let point of this.points) {
-      ctx.fillStyle = "#000000";
+    for (let point of this.points) {
+      ctx.fillStyle = '#000000';
       ctx.beginPath();
       ctx.arc(point.x, point.y, pointRadius, 0, 2 * Math.PI);
       ctx.fill();
-      ctx.fillStyle = "#FFFFFF";
+      ctx.fillStyle = '#FFFFFF';
       ctx.beginPath();
       ctx.arc(point.x, point.y, pointRadius / 2, 0, 2 * Math.PI);
       ctx.fill();
     }
-    
   }
 
-  draw0Label(ctx) {
+  draw0Label(ctx) {}
 
+  draw100Label(ctx) {}
+
+  checkClickedPoint(x, y, shiftWhenDown) {
+    for (let idx = 0; idx < this.points.length; ++idx) {
+      const point = this.points[idx];
+      if (Math.abs(point.x - x) < pointRadius && Math.abs(point.y - y) < pointRadius) {
+        this.selectedPoint = point;
+        this.shiftWhenDown = shiftWhenDown;
+        return true;
+      }
+    }
+    this.selectedPoint = null;
+    return false;
   }
 
-  draw100Label(ctx) {
-
+  checkMove(dx, dy) {
+    if (this.selectedPoint) {
+      if (this.shiftWhenDown) {
+        for (let point of this.points) {
+          point.x += dx;
+          point.y += dy;
+        }
+      } else {
+        this.selectedPoint.x += dx;
+        this.selectedPoint.y += dy;
+      }
+      update();
+    }
   }
 
-  checkClickedPoint(x, y) { 
-
+  update() {
+    this.calculateAngle();
+    this.readDTEInput();
   }
+
+  calculateAngle() {
+    const point0 = this.points[0];
+    const centerPoint = this.points[1];
+    const point100 = this.points[2];
+    const currentPoint = this.points[3];
+
+    const v0 = { x: point0.x - centerPoint.x, y: point0.y - centerPoint.y };
+    const v0Mag = vectorMagnitude(v0);
+    v0.x /= v0Mag;
+    v0.y /= v0Mag;
+
+    const v100 = { x: point100.x - centerPoint.x, y: point100.y - centerPoint.y };
+    const v100Mag = vectorMagnitude(v100);
+    v100.x /= v100Mag;
+    v100.y /= v100Mag;
+
+    const vCurrent = { x: currentPoint.x - centerPoint.x, y: currentPoint.y - centerPoint.y };
+    const vCurrentMag = vectorMagnitude(vCurrent);
+    vCurrent.x /= vCurrentMag;
+    vCurrent.y /= vCurrentMag;
+
+    const totalAngle = vectorAngle(v0, v100);
+    const thisAngle = vectorAngle(v0, vCurrent);
+
+    this.anglePercent = thisAngle / totalAngle;
+  }
+
+  readDTEInput() {
+    this.dte = parseInt(this.dteInput.value);
+  }
+}
+
+function vectorMagnitude(v) {
+  return Math.sqrt(v.x * v.x + v.y * v.y);
+}
+
+function vectorDotProduct(v1, v2) {
+  return v1.x * v2.x + v1.y * v2.y;
+}
+
+function vectorAngle(v1, v2) {
+  return Math.acos(vectorDotProduct(v1, v2));
 }
--- gasTracker/js/gasTracker.js
+++ gasTracker/js/gasTracker.js
@@ -1,5 +1,6 @@
 let wrapperDiv;
 let gauges = [];
+let readings = [];
 
 function init() {
   wrapperDiv = document.getElementById('mainDiv');
@@ -12,6 +13,7 @@ function loadImage(source) {
   const newGauge = new Gauge();
   gauges.push(newGauge);
   dteInput.type = 'number';
+  dteInput.onchange = () => update();
 
   const fileReader = new FileReader();
 
@@ -23,21 +25,66 @@ function loadImage(source) {
       newCanvas.width = tempImage.width;
       newCanvas.height = tempImage.height;
       newGauge.setCenter(newCanvas.width / 2, newCanvas.height / 2);
-      draw();
+      update();
     };
     newCanvas.style.backgroundImage = `url(${data})`;
     newDiv.appendChild(newCanvas);
+    newDiv.appendChild(dteInput);
     wrapperDiv.appendChild(newDiv);
     newGauge.setCanvas(newCanvas);
     newGauge.setDTEInput(dteInput);
+
+    newCanvas.addEventListener('mousedown', ev => {
+      for (let gauge of gauges) {
+        gauge.selectedPoint = null;
+      }
+      for (let gauge of gauges) {
+        if (gauge.checkClickedPoint(ev.offsetX, ev.offsetY, ev.shiftKey)) {
+          break;
+        }
+      }
+    });
+
+    newCanvas.addEventListener('mousemove', ev => {
+      for (let gauge of gauges) {
+        gauge.checkMove(ev.movementX, ev.movementY);
+      }
+    });
+
+    newCanvas.addEventListener('mouseup', () => {
+      for (let gauge of gauges) {
+        gauge.selectedPoint = null;
+      }
+    })
 
   });
 
   fileReader.readAsDataURL(source.files[0]);
 }
 
+function update() {
+  readings = [];
+
+  for (let gauge of gauges) {
+    gauge.update();
+    readings.push({angle: gauge.anglePercent, dte: gauge.dte});
+  }
+
+  console.log(readings);
+
+  draw();
+}
+
 function draw() {
   for (let gauge of gauges) {
     gauge.draw();
   }
+}
+
+function downloadCSV() {
+  let csv = 'Gauge Angle,DTE\n';
+  for (let reading of readings) {
+    csv += `${reading.angle},${reading.dte}\n`;
+  }
+  download(csv, 'stats.csv');
 }
